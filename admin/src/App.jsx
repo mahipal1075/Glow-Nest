1a6640c5d3dc95fb01fff0a787b17a7d1fc8edc5
Mahiaplsinh <mahipalsing123@gmai.com>
2026-09-07 17:44:52 +0530

    Fix case sensitivity for Orders.jsx import in admin App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from './components/sidebar/sidebar.jsx'
 import { Routes,Route } from 'react-router-dom'
 import Add from './pages/add/Add.jsx'
 import List from './pages/list/List.jsx'
-import Orders from './pages/orders/orders.jsx'
+import Orders from './pages/orders/Orders.jsx'
 // import { ToastContainer } from 'react-toastify';
 
 const App = () => {
