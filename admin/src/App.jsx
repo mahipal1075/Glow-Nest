9b22774f6828036300fc7e66701bf7b4003180ae
mahipal1075 <167430885+mahipal1075@users.noreply.github.com>
2026-09-07 18:14:07 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar.jsx'
 import Sidebar from './components/sidebar/sidebar.jsx'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Add from './pages/add/Add.jsx'
 import List from './pages/list/List.jsx'
 import Orders from './pages/orders/Orders.jsx'
-// import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = import.meta.env.VITE_BACKEND_URL || "https://glow-nest-dzvu.onrender.com/";
+  const url = import.meta.env.VITE_BACKEND_URL || "https://glow-nest-dzvu.onrender.com";
 
   return (
     <div>
-      {/* <ToastContainer/> */}
-      <Navbar/>
-      <hr/>
+      <ToastContainer/>
+      <Navbar />
+      <hr />
       <div className='app-content'>
-        <Sidebar/>
+        <Sidebar />
         <Routes className="app-main-page" >
-          <Route path="/add" element={<Add url={url} />}/>
-          <Route path="/list" element={<List url={url} />}/>
-          <Route path="/orders" element={<Orders url={url} />}/>
+          <Route path="/" element={<Navigate to="/list" replace />} />
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
