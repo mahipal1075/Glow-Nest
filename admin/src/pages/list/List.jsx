8904a4a800b39af83378150f7d274014698d4acb
mahipal1075 <167430885+mahipal1075@users.noreply.github.com>
2026-09-07 18:32:28 +0530

    Fix image source URL in List component

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -49,7 +49,7 @@ const List = ({url}) => {
         {list.map((item, index) => {
           return (
             <div className='list-table-format' key={index}>
-              <img src={`${url}image/` + item.image} alt="" />
+              <img src={`${url}image` + item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>Rs.{item.price}/-</p>
